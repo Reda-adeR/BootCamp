3186778563d483b93b613d272a7be794b580dee0
Ano <useranonyme317@gmail.com>
2025-05-15 15:31:23 +0100

    More Querying using PGadmin, SQLTools, before using Python as client

---
+++ week2/day2/ex1.sql
@@ -0,0 +1,9 @@
+
+SELECT * FROM items ORDER BY item_price;
+
+SELECT * FROM items WHERE item_price >= 150 ORDER BY item_price DESC;
+
+SELECT item_name, item_price FROM items ORDER BY item_name LIMIT 3;
+
+SELECT item_name FROM items ORDER BY item_name DESC;
+
